ff332f2d9fb6b2004066ad7a2d40070e25506942
vincentsanjaya <47843686+vincentsanjaya@users.noreply.github.com>
2019-07-25 18:33:17 +0800

    explanation
    fethtemp.js
    index.js
    full explanation

--- raspberrypi-pubsub/fetchTemp.js
+++ raspberrypi-pubsub/fetchTemp.js
@@ -1,17 +1,17 @@
-const spawn = require('child_process').spawn;
-const file = __dirname + '/temp.py'
+const spawn = require('child_process').spawn; //call the function of child_process.spawn
+const file = __dirname + '/temp.py' //define the file path
 
-const temp = async () => {
+const temp = async () => { //node run in asynchronous process creation
   const p = new Promise((res, rej) => {
     // Spawn a child and read the data back
-    let client = spawn('python', [file], {cwd: '/tmp'});
-    client.stdout.on('data', (data) => {
-       res(data.toString())
+    let client = spawn('python', [file], {cwd: '/tmp'}); // spawn the child on client var
+    client.stdout.on('data', (data) => { //stdout = output of the child process
+       res(data.toString())  // the raw data put into the 'data'
     });
 
   })
   return p
 }
 
-module.exports = temp
+module.exports = temp //run the node
 
--- raspberrypi-pubsub/index.js
+++ raspberrypi-pubsub/index.js
@@ -1,9 +1,9 @@
-const temp = require('./fetchTemp.js')
-const { init } = require('./zmqWatcher.js')
+const temp = require('./fetchTemp.js') //use the function converted input data from python to Javascript
+const { init } = require('./zmqWatcher.js') //use the function ZMQ for subscribe the tangle
 
 /// IOTA Setup
-const IOTA = require('@iota/core');
-const Converter = require('@iota/converter');
+const IOTA = require('@iota/core'); 
+const Converter = require('@iota/converter'); //Using the iota converter library
 const iota = IOTA.composeAPI({ provider: 'https://nodes.devnet.thetangle.org:443' }); // change this by checking public node service of IOTA
 const iotaSeed =
   'PUEOTSEITFEVEWCWBTSIZM9NKRGJEIMXTULBACGFRQK9IMGICLBKW9TTEVSDQMGWKBXPVCBMMCXWMNPDX';// you can use your own seed
@@ -14,7 +14,7 @@ console.log("Starting app");
 
 /// Send IOTA Transaction
 const sendTx = data => {
-  const message = Converter.asciiToTrytes(JSON.stringify({ temperature: data }))
+  const message = Converter.asciiToTrytes(JSON.stringify({ temperature: data })) //convert the JSON data to string and convert from ascii into trytes
   const transactions = [ //prepare the transaction in JSON
     {
       value: 0,
@@ -25,13 +25,13 @@ const sendTx = data => {
 
   iota
     .prepareTransfers(iotaSeed, transactions) //prepareTransfer(own_seed, data)
-    .then(trytes => iota.sendTrytes(trytes, 3, 9))
+    .then(trytes => iota.sendTrytes(trytes, 3, 9)) //iota.sendTrytes(trytes,/*depth*/,/*minimum weight magnitude*/)
     .then(bundle => {
       console.log('Transfer successfully sent')
       bundle.map(tx => console.log(tx))
     })
-    .catch(err => { //error exception and show error
-      console.log(err)
+    .catch(err => { //catch the error 
+      console.log(err) //show the error
     })
 }
 
@@ -48,7 +48,7 @@ const sendTemp = async () => {
       message: message // The message converted into trytes
     }
   ]
-  sendTx(transactions)
+  sendTx(transactions) // send to the node
 }
 
 const sendAddress = () => {
